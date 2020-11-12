fc3861b39abe603565249c5092f8ad79d84053aa
Anubhav Sethi <32430170+LifeofAGeek@users.noreply.github.com>
2020-11-12 16:45:36 +0530

    https://atcoder.jp/contests/abc182/tasks/abc182_c

---
+++ To3.cpp
@@ -0,0 +1,77 @@
+ #include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define loop(i, start, end) for (int i = start; i <= end; i++)
+#define rloop(i, start, end) for (int i = start; i >= end; i--)
+#define vi vector<int>
+#define vec(x) vector<x>
+#define sz(v) (int)v.size() 
+#define UB upper_bound
+#define LB lower_bound
+#define all(v) (v).begin(),(v).end()
+#define arl(v) (v).rbegin(),(v).rend()
+#define fsp(a) fixed<<setprecision(a)
+#define mem(a,with) memset(a, with, sizeof (a))
+#define vmn(a) (*min_element(a.begin(),a.end()))   
+#define vmx(a) (*max_element(a.begin(),a.end()))
+#define bs(a,key) binary_search(all(a),key)    /// return bool.
+#define rotl(a,x) rotate(a.begin(),a.begin()+x,a.end());
+#define rotr(a,x) rotl(a,a.size()-x);
+#define nl cout<<endl
+#define MOD 1000000007
+int add(int x, int y) {int res = x + y; return (res >= MOD ? res - MOD : res);}
+int mul(int x, int y) {int res = x * y; return (res >= MOD ? res % MOD : res);}
+int power(int x, int y) {int res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+
+int n;
+string s;
+int dp[20][200];
+ 
+int helper(int i, int sum)
+{
+    if(i == n)
+    {
+        if(sum%3 == 0)
+            return 0;
+        else
+            return 1e6;
+    }
+    int &ans = dp[i][sum];
+    if(ans != -1)
+        return ans;
+ 
+    ans = 0;
+    int add = s[i] - '0';
+    ans = min(helper(i+1, sum+add), 1 + helper(i+1, sum));
+ 
+    return ans;
+ 
+}
+
+void solve(){
+    //code goes here
+    cin>>s;
+    n=s.size();
+    mem(dp,-1);
+    int del=helper(0,0);
+    if(del==n) cout<<-1;
+    else cout<<del;
+}
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ios_base::sync_with_stdio(false);
+cout.tie(NULL);
+cin.tie(NULL);
+    int t=1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
