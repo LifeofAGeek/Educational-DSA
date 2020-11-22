b6378c9c2466ad9a867ceeef47e7109ffe0560f9
Anubhav Sethi <32430170+LifeofAGeek@users.noreply.github.com>
2020-11-23 02:15:57 +0530

    digit dp - digit sum equal to X

---
+++ digit-dp/digit_sum.cpp
@@ -0,0 +1,85 @@
+// count numbers in the range [0,R] where R<=1e18 which have digit sum equal to X.
+#include<bits/stdc++.h>
+using namespace std::chrono;
+using namespace std;
+#define int long long
+#define loop(i, start, end) for (int i = start; i <= end; i++)
+#define rloop(i, start, end) for (int i = start; i >= end; i--)
+#define read(a,n) loop(i,0,n-1) cin>>a[i];
+#define vi vector<int>
+#define vec(x) vector<x>
+#define sz(v) (int)v.size() 
+#define UB upper_bound
+#define LB lower_bound
+#define all(v) (v).begin(),(v).end()
+#define arl(v) (v).rbegin(),(v).rend()
+#define fsp(a) fixed<<setprecision(a)
+#define mem(a,with) memset(a, with, sizeof (a))
+#define vmn(a) (*min_element(a.begin(),a.end()))   
+#define vmx(a) (*max_element(a.begin(),a.end()))
+#define bs(a,key) binary_search(all(a),key)    /// return bool.
+#define rotl(a,x) rotate(a.begin(),a.begin()+x,a.end());
+#define rotr(a,x) rotl(a,a.size()-x);
+#define nl cout<<endl
+#define dbg(x) cerr << #x << " :: " << x << endl;
+#define dbg2(x, y) cerr << #x << " :: " << x << "\t" << #y << " :: " << y << endl;
+#define MOD 1000000007
+int add(int x, int y) {int res = x + y; return (res >= MOD ? res - MOD : res);}
+int mul(int x, int y) {int res = x * y; return (res >= MOD ? res % MOD : res);}
+int power(int x, int y) {int res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+
+string r;
+int sum;
+int dp[20][200][2];
+
+
+int f(string& num, int n, int x, bool tight){
+    //base cases
+    if(n==1){
+        if(tight and x>=0 and x<=(num[num.size()-n]-'0')) return 1;
+        else if(!tight and x>=0 and x<=9) return 1;
+        else return 0;
+    }
+
+    int &ans=dp[n][x][tight];
+    if(ans!=-1) return ans;
+
+    ans=0;
+    int ub=tight?(num[num.size()-n]-'0'):9;
+    loop(dig,0,ub){
+        ans+=f(num,n-1,x-dig,(tight&(dig==ub)));
+    }
+
+    return ans;
+}
+
+void solve(){
+    //code goes here
+    cin>>r>>sum;
+    mem(dp,-1);
+    cout<<f(r,r.size(),sum,1);
+}
+
+int32_t main()
+{
+auto start = chrono::high_resolution_clock::now();
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ios_base::sync_with_stdio(false);
+cout.tie(NULL);
+cin.tie(NULL);
+    int t=1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+#ifndef ONLINE_JUDGE
+     auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<microseconds>(stop - start);
+    cout<<"\n\nExecuted In: "<<duration.count()<<" ms";
+#endif
+    return 0;
+}
