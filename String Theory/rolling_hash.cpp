a6048bd374c52cea9493aa571962c1e2fce05a31
Anubhav Sethi <32430170+LifeofAGeek@users.noreply.github.com>
2021-07-28 11:50:37 +0530

    Update rolling_hash.cpp

--- String Theory/rolling_hash.cpp
+++ String Theory/rolling_hash.cpp
@@ -1,22 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-const int MOD = 1e9+7;
-
-int rolling_hash(string& s){
-    int hash = 0, p = 31, p_power = 1; // A prime p >= |character set|
-    for(char& ch:s){
-        hash = (hash + (ch-'a'+1)*p_power)%MOD;
-        p_power = (p*p_power)%MOD;
-    }
-    return hash;
-}
 
 void solve(){
     //code goes here
-    string s;
-    cin>>s;
-    cout<<rolling_hash(s);
+    int p = 31;
+    string s = "ab";
+    string t = "abcabcab";
+    const int n = s.size(), m = t.size(), MOD = 1e9+7;
+    vector<int> power(m), hashT(m + 1, 0);
+    power[0] = 1;
+    for(int i = 1; i < m; i++) power[i] = (power[i - 1] * p) % MOD;
+
+    for(int i = 1; i <= m; i++){
+        hashT[i] = (hashT[i - 1] + ((t[i - 1] - 'a' + 1) * power[i - 1]) % MOD) % MOD;
+    }
+
+    int hashS = 0;
+    for(int i = 0; i < n; i++){
+        hashS = (hashS + ((s[i] - 'a' + 1) * power[i]) % MOD) % MOD;
+    }
+
+    for(int i = 0; i + n - 1 < m; i++){
+        int curr_hash = (hashT[i + n] - hashT[i] + MOD) % MOD;
+        if(curr_hash  == (hashS * power[i]) % MOD) cout << i << " ";
+    }
 }
 
 signed main()
